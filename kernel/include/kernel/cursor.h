04c7e384f8740b24aebd84b58f2c8a5010610101
Nesmy <pnesmy@gmail.com>
2025-05-30 23:35:51 -0400

    start

--- kernel/include/kernel/cursor.h
+++
@@ -1,9 +0,0 @@
-#ifndef _KERNEL_CURSOR_H
-#define _KERNEL_CURSOR_H
-
-#include <stddef.h>
-#include <stdint.h>
-
-void fb_move_cursor(unsigned short pos);
-
-#endif
--- kernel/include/kernel/timer.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIMER_H_
-#define _TIME_H_
-
-#include "../arch/i386/io.h"
-
-void init_timer(uint32_t frequency);
-void on_Irq0(struct InterruptRegisters* regs);
-
-#endif
