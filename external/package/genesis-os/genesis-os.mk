5e6b54c1aacdc7bb7b22269b62c47c4226fc5eb1
Nesmy <pnesmy@gmail.com>
2025-08-04 08:14:15 -0400

    update 2

--- external/package/genesis-os/genesis-os.mk
+++ external/package/genesis-os/genesis-os.mk
@@ -1,10 +1,11 @@
-GENESIS_OS_VERSION = 5c6b78e7327bdd5dd852cfa498cdc6c49aa5ddb8
+GENESIS_OS_VERSION = abb2e486896603dc91cc3452a9fd3980cb107395 
 GENESIS_OS_SITE = https://github.com/Genesis-Grid-Lab/Genesis-OS.git
 GENESIS_OS_SITE_METHOD = git
 GENESIS_OS_GIT_SUBMODULES = YES
 GENESIS_OS_DEPENDENCIES = mesa3d libdrm libgbm libgles libegl
 
-GENESIS_OS_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
+GENESIS_OS_CONF_OPTS = -DCMAKE_BUILD_TYPE=Debug \
+                       -DCMAKE_INSTALL_PREFIX=/usr
 
 define GENESIS_OS_PRE_INSTALL
 	mkdir -p $(HOST_DIR)/$(GNU_TARGET_NAME)/sysroot/usr
@@ -13,15 +14,16 @@ endef
 GENESIS_OS_POST_PATCH_HOOKS += GENESIS_OS_PRE_INSTALL
 
 define GENESIS_OS_CONFIGURE_CMDS
-    cd $(@D) && cmake -DCMAKE_INSTALL_PREFIX=/usr .
+	cd $(@D) && cmake $(GENESIS_OS_CONF_OPTS) .
 endef
 
 define GENESIS_OS_BUILD_CMDS
-    $(MAKE) -C $(@D)
+	$(MAKE) -C $(@D)
 endef
 
-define GENESIS_UI_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/genesis-ui $(TARGET_DIR)/usr/bin/genesis-ui
+define GENESIS_OS_INSTALL_TARGET_CMDS
+	DESTDIR=$(TARGET_DIR) $(MAKE) -C $(@D) install
 endef
 
 $(eval $(cmake-package))
+
